b1169a5394e8d831fd2e24fa71822b0c63b06d17
Stavre Alin <31391614+Stavre@users.noreply.github.com>
2021-02-28 10:34:22 +0200

    Update sqrt.c
    Rudimentary implementation of Newton's method for finding the square root of real numbers. The function has a precision of six digits.

--- sqrt.c
+++ sqrt.c
@@ -5,29 +5,26 @@ double square_root(long double i)
 {
     //calculating sqrt(i) using newton's method
 
-    long double x = (double)i/3;
+    long double x = i/3; //initial estimation (not a very good one)
     long double y = -1;
     int j = 0;
-    while (j < 51)
-    {
-        //y = x;
-        x = 0.5*(x+((double)i)/x);
-        //x = 0.5*x*(3 - (double)i*x*x);
+    while (j < 51) //51 iterations
+    {      
+        x = 0.5*(x+((double)i)/x);   
         j = j+1;
-       //printf("%f %f\n", x, y);
     }
-
-    //x = (long double)i*x;
     return x;
 }
 
 int main()
 {
-   /* printf("\n hello \n");
+    //code used for comparison with sqrtl
+   /* 
     for(long double i = 1; i < 123456789; i++)
     {
         double a = square_root(i);
         double b = sqrtl(i);
+        //keeping only the first ten digits
         a = a*10;
         a = trunc(a);
         a = a / 10;
@@ -42,8 +39,7 @@ int main()
         }
     }*/
     printf("%lf \n", square_root(9.36));
-    printf("Succes");
+
     return 0;
 }
-
-//compile with  gcc -o foo foo.c -lm
+//compile with  gcc -o foo foo.c -lm 
